b35f5dab150f80498eba00c717604f8b4caf3c5c
Dhina17 <dhinalogu@gmail.com>
2022-07-24 17:16:04 +0000

    android: setup: sh -> bash

--- android/setup.sh
+++ android/setup.sh
@@ -1,7 +1,7 @@
 # Initital setup
 # Install required packages.
 # Credits - https://github.com/akhilnarang/scripts
-sh -c "$(wget -O- https://github.com/akhilnarang/scripts/raw/master/setup/android_build_env.sh)"
+bash -c "$(wget -O- https://github.com/akhilnarang/scripts/raw/master/setup/android_build_env.sh)"
 
 # Pull git config
 wget -O ~/.gitconfig https://github.com/Dhina17/dotfiles/raw/main/.gitconfig
