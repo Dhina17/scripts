ac188dfbfe125c671840dd252a6f28d0c4c1a076
Dhina17 <dhinalogu@gmail.com>
2022-07-25 04:22:16 +0000

    android: setup: Add gdrive tool

--- android/setup.sh
+++ android/setup.sh
@@ -6,3 +6,8 @@ bash -c "$(wget -O- https://github.com/akhilnarang/scripts/raw/master/setup/andr
 # Pull git config
 wget -O ~/.gitconfig https://github.com/Dhina17/dotfiles/raw/main/.gitconfig
 
+# Setup gdrive tool
+wget https://raw.githubusercontent.com/usmanmughalji/gdriveupload/master/gdrive
+chmod +x gdrive
+sudo install gdrive /usr/local/bin/gdrive
+rm gdrive
