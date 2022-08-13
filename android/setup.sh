896ed58fc25d8689ba606628ee35c00938f147d0
Dhina17 <dhinalogu@gmail.com>
2022-08-14 02:18:31 +0530

    android: setup: Fixup if

--- android/setup.sh
+++ android/setup.sh
@@ -36,7 +36,7 @@ rm gdrive
 # Add custom function to handle gdrive shared drive
 # Usage gu <file_name>
 CUS_FUNC="gu() { gdrive upload --share \"\${1:?Error: Please give file name}\"  --parent $GDRIVE_PARENT_ID }"
-if [[ $(basename $SHELL) == "zsh"]]; then
+if [ $(basename $SHELL) == "zsh" ]; then
     echo $CUS_FUNC >> ~/.zshrc
     source ~/.zshrc
 else
