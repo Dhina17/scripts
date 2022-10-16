2241f63aa5a64711d3737383f3083cffa465d202
Dhina17 <dhinalogu@gmail.com>
2022-10-16 21:08:04 +0530

    setup: Install some cool zsh plugins

--- setup.sh
+++ setup.sh
@@ -13,4 +13,10 @@ chsh -s $(which zsh)
 # Install oh-my-zsh
 sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
+# Install cool plugins to make life easier.
+# Install zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git -b master ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
+# Install zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-autosuggestions.git -b master ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 
