223dde16746af588494753c724c7076ec9f9104a
Dhina17 <dhinalogu@gmail.com>
2024-04-13 10:41:11 +0530

    setup: Install plugins after ohmyzsh installation

--- setup.sh
+++ setup.sh
@@ -15,16 +15,16 @@ mkdir -p ~/.config/micro/colorschemes/
 wget -O ~/.config/micro/colorschemes/dracula.micro https://github.com/Dhina17/dotfiles/raw/main/.config/micro/colorschemes/dracula.micro
 wget -O ~/.config/micro/settings.json https://github.com/Dhina17/dotfiles/raw/main/.config/micro/settings.json
 
-# Install cool plugins to make life easier.
-# Install zsh-syntax-highlighting
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git -b master ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-
-# Install zsh-autosuggestions
-git clone https://github.com/zsh-users/zsh-autosuggestions.git -b master ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-
 # Install and setup ZSH
 sudo apt install zsh
 chsh -s $(which zsh)
 
 # Install oh-my-zsh
 sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# Install cool plugins to make life easier.
+# Install zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git -b master ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
+# Install zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-autosuggestions.git -b master ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
